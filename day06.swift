de2799ef8228c44b8916b8a3f7a71eb3a4b8d6e7
Kilian Brendel <kilian.brendel@posteo.de>
2023-12-01 22:29:51 +0100

    Comments and polishing

--- day06.swift
+++ day06.swift
@@ -1,34 +1,31 @@
 import Foundation
 
-func findMarker(_ message : String.SubSequence, markerLen : Int) -> Int {
-    var seen : [Character : Int] = [:]
-    var minValidPos = 0
-    for (i, ch) in message.enumerated() {
-        // If character occured before, may have to increase the minimum
-        // possible position
-        minValidPos = max(minValidPos, (seen[ch] ?? -1) + markerLen)
-        seen[ch] = i
-        if i >= minValidPos {
-            // Return 1-based index of last processed character
-            return i + 1
-        }
+func findMarker(_ message: String.SubSequence, markerLen: Int) -> Int {
+  var seen: [Character: Int] = [:]
+  var firstValidPos = markerLen
+  for (i, ch) in message.enumerated() {
+    // If the character has occured before, update minimum acceptable position
+    firstValidPos = max(firstValidPos, (seen[ch] ?? -1) + markerLen)
+    if i >= firstValidPos {
+      // Return end position of valid marker (1-based)
+      return i + 1
     }
-    return -1
+    seen[ch] = i
+  }
+  return -1
 }
 
 let filename = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : "example06.txt"
-do
-{
-    // Support multiple messages in input file (there are multiple examples)
-    let messages = try String(contentsOfFile: filename).split(separator: "\n")
-    for message in messages {
-        if (message.count < 100)
-        {
-            print(message)
-        }
-        print("  Start of packet:  \(findMarker(message, markerLen: 4))")
-        print("  Start of message: \(findMarker(message, markerLen: 14))")
+do {
+  // Support multiple messages in input file (there are multiple examples), process one line at a time
+  let messages = try String(contentsOfFile: filename).split(separator: "\n")
+  for message in messages {
+    if messages.count > 1 {
+      print(message)
     }
+    print("  Start of packet: ", findMarker(message, markerLen: 4))
+    print("  Start of message:", findMarker(message, markerLen: 14))
+  }
 } catch {
-    print("Could not load input from '\(filename)'.")
+  print("Could not load input from '\(filename)'.")
 }
